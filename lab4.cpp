09dc275ad9f618906ae0164d94a13c8ee80fbc29
KonstantinosStefanidis <viviotic01@gmail.com>
2025-12-19 10:41:31 +0200

    did function avgOfNeg

--- lab4.cpp
+++ lab4.cpp
@@ -8,8 +8,9 @@ using namespace std;
 
 //Prototypes
 void enterValues(double nums[], const int size, const double r_min, const double r_max);
+double avgOfNeg(const double nums[], const int size);
 /**
- * 
+ * Start of the program
  */
 int main(){
     const double R_MIN = -1.9;
@@ -23,14 +24,21 @@ int main(){
         cout << "\n2) Calc. avg. of nevagtive";
         cout << "\n0) Exit";
         cout << "\nEnter choice: ";
+        bool first = false;
         cin >> choice;
 
         switch (choice)
         {
         case 1:
-        enterValues(nums, SIZE, R_MIN, R_MAX);
+            enterValues(nums, SIZE, R_MIN, R_MAX);
+            first = true;
             break;
         case 2:
+            if(!first){
+                cerr << "\nMust use option 1 first!\n";
+            } else {
+                cout << "\nThe avg of neg. are: " << avgOfNeg(nums, SIZE) << endl;
+            }
             break;
         case 0:
             break;
@@ -61,4 +69,30 @@ void enterValues(double nums[], const int size, const double r_min, const double
             cin >> nums[i];
         }while (nums[i] < r_min || nums[i] > r_max);
     }
+}
+
+/**
+ * Returns the average of the negative numbers of a given array
+ * 
+ * @param nums the array
+ * @param size the size of the array
+ * @return returns the average of the negative numbers
+ */
+double avgOfNeg(const double nums[], const int size){
+    assert(size > 0);
+
+    double sum = 0;
+    int count = 0;
+
+    for (int i =0; i < size; i++){
+        if (nums[i] < 0) {
+            sum += nums[i];
+            count++;
+        }
+    }
+    if (count > 0){
+        sum /= count;
+    }
+
+    return sum;
 }
